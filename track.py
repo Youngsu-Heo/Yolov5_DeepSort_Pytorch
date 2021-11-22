59f2279406d1a621d86ee4b592069884a8aa7cca
Leo <ysheo1207@gmail.com>
2021-11-22 10:33:19 +0900

    customize for motorcycle detection

--- track.py
+++ track.py
@@ -33,8 +33,14 @@ def detect(opt):
     out, source, yolo_weights, deep_sort_weights, show_vid, save_vid, save_txt, imgsz, evaluate, half = \
         opt.output, opt.source, opt.yolo_weights, opt.deep_sort_weights, opt.show_vid, opt.save_vid, \
             opt.save_txt, opt.imgsz, opt.evaluate, opt.half
+    
     webcam = source == '0' or source.startswith(
         'rtsp') or source.startswith('http') or source.endswith('.txt')
+
+    # motorcycle related variables
+    mc_count = 0
+    mc_ids = {}
+
 
     # initialize deepsort
     cfg = get_config()
@@ -97,82 +103,123 @@ def detect(opt):
     if pt and device.type != 'cpu':
         model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
+
+
+    start_time = time.time()
+
     for frame_idx, (path, img, im0s, vid_cap, s) in enumerate(dataset):
-        t1 = time_sync()
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
-        t2 = time_sync()
-        dt[0] += t2 - t1
+        # print(f'frame_idx = {frame_idx}')
+        if frame_idx % opt.period == 0:            
+            t1 = time_sync()
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+            t2 = time_sync()
+            dt[0] += t2 - t1
 
-        # Inference
-        visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if opt.visualize else False
-        pred = model(img, augment=opt.augment, visualize=visualize)
-        t3 = time_sync()
-        dt[1] += t3 - t2
+            # Inference
+            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if opt.visualize else False
+            pred = model(img, augment=opt.augment, visualize=visualize)
+            t3 = time_sync()
+            dt[1] += t3 - t2
 
-        # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
-        dt[2] += time_sync() - t3
+            # Apply NMS
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
+            dt[2] += time_sync() - t3
 
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            seen += 1
-            if webcam:  # batch_size >= 1
-                p, im0, frame = path[i], im0s[i].copy(), dataset.count
-                s += f'{i}: '
-            else:
-                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                seen += 1
+                if webcam:  # batch_size >= 1
+                    p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                    s += f'{i}: '
+                else:
+                    p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
 
-            s += '%gx%g ' % img.shape[2:]  # print string
-            save_path = str(Path(out) / Path(p).name)
+                s += '%gx%g ' % img.shape[2:]  # print string
+                save_path = str(Path(out) / Path(p).name)
 
-            annotator = Annotator(im0, line_width=2, pil=not ascii)
+                annotator = Annotator(im0, line_width=2, pil=not ascii)
 
-            if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(
-                    img.shape[2:], det[:, :4], im0.shape).round()
+                if det is not None and len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(
+                        img.shape[2:], det[:, :4], im0.shape).round()
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-                xywhs = xyxy2xywh(det[:, 0:4])
-                confs = det[:, 4]
-                clss = det[:, 5]
+                    xywhs = xyxy2xywh(det[:, 0:4])
+                    confs = det[:, 4]
+                    clss = det[:, 5]
 
-                # pass detections to deepsort
-                outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
-                
-                # draw boxes for visualization
-                if len(outputs) > 0:
-                    for j, (output, conf) in enumerate(zip(outputs, confs)): 
-                        
-                        bboxes = output[0:4]
-                        id = output[4]
-                        cls = output[5]
+                    # pass detections to deepsort
+                    outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
+                    
+                    # draw boxes for visualization
+                    if len(outputs) > 0:
+                        for j, (output, conf) in enumerate(zip(outputs, confs)): 
+                            
+                            bboxes = output[0:4]
+                            id = output[4]
+                            cls = output[5]
+    #                        print(output)
 
-                        c = int(cls)  # integer class
-                        label = f'{id} {names[c]} {conf:.2f}'
-                        annotator.box_label(bboxes, label, color=colors(c, True))
+                            
+                            # if not(id in mc_ids) and cls == 3:
+                            #     mc_ids[id] = 1
+                            #     mc_count += 1
+                            #     print(f'mc_ids = {mc_ids}')
 
-                        if save_txt:
-                            # to MOT format
-                            bbox_left = output[0]
-                            bbox_top = output[1]
-                            bbox_w = output[2] - output[0]
-                            bbox_h = output[3] - output[1]
-                            # Write MOT compliant results to file
-                            with open(txt_path, 'a') as f:
-                               f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,
-                                                           bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))  # label format
+                            if cls == 3:
+                                print(f'motorcycle detected: {output}')
+                                if not(id in mc_ids):
+                                    mc_ids[id] = 1
+                                else:
+                                    mc_ids[id] += 1
+                                    if mc_ids[id] == opt.cthres:
+                                        mc_count += 1
+                                        print(f'mc_ids = {mc_ids}')
 
-            else:
-                deepsort.increment_ages()
+                            # FIXME: 화면에 10회 등장한 객체만 카운트 (FIXME: 10회 연속 등장으로 조건 바꾸기
+                            # 연속등장하지 않은 객체는 딕셔너리에서 제거하기. 10회 이상)
+                            # 10회 등장한 id는 done 딕셔너리로 옮기고 재등장 시 카운트하지 않기
+
+
+
+
+                            c = int(cls)  # integer class
+                            label = f'{id} {names[c]} {conf:.2f}'
+                            annotator.box_label(bboxes, label, color=colors(c, True))
+
+                            if save_txt:
+                                # to MOT format
+                                bbox_left = output[0]
+                                bbox_top = output[1]
+                                bbox_w = output[2] - output[0]
+                                bbox_h = output[3] - output[1]
+                                # Write MOT compliant results to file
+                                with open(txt_path, 'a') as f:
+                                   # f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,
+                                   #                             bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))  # label format
+
+                                   if cls == 3:
+                                       label_format = ('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,
+                                                                   bbox_top, bbox_w, bbox_h, -1, -1, -1, -1)
+                                       # print(label_format)
+                                       f.write(f'{frame_idx}, {cls}, {label_format}')  # label format
+
+
+                else:
+                    deepsort.increment_ages()
+
+            now_time = time.time()
+            elapsed = now_time - start_time
+            print(f'fps = {(frame_idx + 1) / elapsed},  mc_count = {mc_count}')
 
             # Print time (inference-only)
             LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
@@ -198,7 +245,9 @@ def detect(opt):
                         fps, w, h = 30, im0.shape[1], im0.shape[0]
                         save_path += '.mp4'
 
-                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    # vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    # vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), 15, (w/2, h/2))
+                    vid_writer = cv2.cudacodec.createVideoWriter(save_path, (w, h), fps)
                 vid_writer.write(im0)
 
     # Print results
@@ -235,6 +284,8 @@ if __name__ == '__main__':
     parser.add_argument('--visualize', action='store_true', help='visualize features')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detection per image')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--period', type=int, default=1, help='frame skip peroid')
+    parser.add_argument('--cthres', type=int, default=5, help='threshold of occurance to count object')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
 
